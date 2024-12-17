c5f2fe68ec0f2257e1fd228cf3cb493bee630a40
Jun <87404676+Seungjun0906@users.noreply.github.com>
2024-12-17 16:51:20 +0900

    테스트

---
+++ day_1.py
@@ -0,0 +1,11 @@
+str = "Life is to short"
+
+# formatted_str = "this is a %-20s" % "정렬쓰"
+
+formatted_str = f"hello{'승준':=^30}"
+
+print(len(formatted_str))
+
+print(formatted_str)
+
+print(formatted_str.count("="))
